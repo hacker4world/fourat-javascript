24f38fe5f2f67c2a431ab3765b7c7ec2d3b02710
hacker4world <zonegaming514@gmail.com>
2026-06-26 11:33:30 +0100

    ...

---
+++ exercice.js
@@ -0,0 +1,40 @@
+/*let A = Number(prompt("Enter A"));
+let B = Number(prompt("Enter B"));
+let C = prompt("donner l'opération");
+
+if (C != "+" && C != "-" && C != "*" && C != "**" && C != "%" && C != "/") {
+  alert("NOOOO PLEASESSS");
+} else if (C == "+") {
+  console.log(A + B);
+} else if (C == "-") {
+  console.log(A - B);
+} else if (C == "*") {
+  console.log(A * B);
+} else if (C == "**") {
+  console.log(A ** B);
+} else if (C == "%") {
+  console.log(A % B);
+} else if (C == "/" && (A == 0 || B == 0)) {
+  alert("Eruurrr");
+} else {
+  console.log(A / B);
+}*/
+
+/*for (let i = 1; i <= 100; i++) {
+  if (i % 2 == 0) {
+    console.log(i);
+  }
+}*/
+
+
+
+/*if (isNaN(A) && isNaN(B)) {
+  alert("ta7che  !!");
+} else {
+  console.log(A + B);
+  console.log(A - B);
+  console.log(A * B);
+  console.log(A / B);
+  console.log(A ** B);
+  console.log(A % B);
+}*/
---
+++ script.js
@@ -0,0 +1,35 @@
+let nom = "aymen"; // string
+
+let age = 25;
+
+let salary = 1200.5;
+
+let isMarried = true;
+
+// string operations
+
+console.log(nom[0]);
+console.log(nom.length);
+console.log(nom.startsWith("a"));
+console.log(nom.endsWith("z"));
+console.log(nom.includes("y"));
+console.log(nom.indexOf("z"));
+console.log(nom.toUpperCase());
+console.log(nom.toLowerCase());
+console.log(isNaN(nom));
+
+// number operations
+
+// let userage = prompt("Enter your age : ")
+
+// for i in range(10):
+
+51;
+
+A = Number(prompt("Enter A"));
+while (A % 2 != 0 || A < 50 || A > 70) {
+  A = Number(prompt("Enter A"));
+}
+console.log(A);
+
+let Liste = [];
